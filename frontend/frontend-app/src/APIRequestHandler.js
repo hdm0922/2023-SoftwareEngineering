4339a7108f782ca05dfec5e41336092cb04c02ec
hdm0922 <dmh0922@naver.com>
2023-11-17 21:50:02 +0900

    feat: Add APIRequestHandler

---
+++ frontend/frontend-app/src/APIRequestHandler.js
@@ -0,0 +1,34 @@
+
+
+class APIRequestHandler {
+
+    // temp use
+    static requestData() {
+        
+    }
+
+
+    static requestPath() {
+
+    }
+
+    // static #handler;
+
+    // constructor() {
+    //     if (APIRequestHandler.#handler) return;
+    //     APIRequestHandler.#handler = this;
+    // }
+
+    // static handler() {
+    //     if (!APIRequestHandler.#handler) {
+    //         APIRequestHandler.#handler = new APIRequestHandler();
+    //     }
+    //     return APIRequestHandler.#handler;
+    // }
+
+
+
+}
+
+
+export default APIRequestHandler;
