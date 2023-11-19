4698b93cb7afd2cb8692608cda0315d500a7092c
hdm0922 <dmh0922@naver.com>
2023-11-19 13:36:38 +0900

    feat: Add robot move event

--- frontend/frontend-app/src/APIRequestHandler.js
+++ frontend/frontend-app/src/APIRequestHandler.js
@@ -6,6 +6,14 @@ class APIRequestHandler {
         return null;
     }
     
+
+    static fetchRobotMovement() {
+        return {
+            robotMovement: "Rotate",
+            moveDistance: 0
+        };
+    }
+
     // 서버측에 사용자의 명령을 전달한 후, 응답으로 새로운 입력 데이터(items, path)를 받는다.
     static fetchUpdateData(userOrder) {
         return {
--- frontend/frontend-app/src/mainpanel/MainPanel.js
+++ frontend/frontend-app/src/mainpanel/MainPanel.js
@@ -7,7 +7,9 @@ import SimulatePanel from "./renderpanel/SimulatePanel";
 import ResumeButton from "./renderbutton/ResumeButton";
 import PauseButton from "./renderbutton/PauseButton";
 import GenerateAreaButton from "./renderbutton/GenerateAreaButton";
+
 import Parser from "../Parser";
+import APIRequestHandler from "../APIRequestHandler";
 
 
 const Array2D = function(x, y, value) {
@@ -41,7 +43,8 @@ const MainPanel = function(props) {
     const [areaSizeY, setAreaSizeY] = useState(0);
     const [robotPath, setRobotPath] = useState([]);
     const [itemsToRender, setItemsToRender] = useState(Array2D(areaSizeX, areaSizeY, null));
-
+    const [robotPosition, setRobotPosition] = useState({});
+    const [robotRotationDegree, setrobotRotationDegree] = useState(0);
 
     const setRenderPanelState = function(newPanel) {
 
@@ -87,6 +90,7 @@ const MainPanel = function(props) {
 
         setAreaSizeX( inputAreaSize[0].x+1 );
         setAreaSizeY( inputAreaSize[0].y+1 );
+        setRobotPosition( initRobotPosition[0] );
         setRobotPath( initRobotPath );
 
         const initItemsToRender = Array2D(inputAreaSize[0].x+1, inputAreaSize[0].y+1, '');
@@ -105,6 +109,48 @@ const MainPanel = function(props) {
         setItemsToRender( initItemsToRender );
     };
 
+    const updateItemsToRender = function(itemType, x, y) {
+        const newItemsToRender = [...itemsToRender];
+        newItemsToRender[y][x] = itemType;
+        setItemsToRender(newItemsToRender);
+    }
+
+    const MoveRobot = function() {
+
+        const nextMovement = APIRequestHandler.fetchRobotMovement();
+        
+        switch (nextMovement.robotMovement) {
+
+            case "Stop":
+                break;
+
+            case "Rotate" :
+                setrobotRotationDegree( (robotRotationDegree + 90) % 360 );
+                break;
+
+            case "Move" :
+                
+                updateItemsToRender("", robotPosition.x, robotPosition.y);
+                const newRobotPosition = {x: robotPosition.x, y: robotPosition.y};
+
+                switch (robotRotationDegree) {
+                    case 0      : newRobotPosition.y++; break;
+                    case 90     : newRobotPosition.x++; break;
+                    case 180    : newRobotPosition.y--; break;
+                    case 270    : newRobotPosition.x--; break;
+                    default     :                       break;
+                }
+
+                updateItemsToRender("Robot", newRobotPosition.x, newRobotPosition.y);
+                setRobotPosition(newRobotPosition);
+                break;
+
+            default :
+                break;
+        }
+    };
+
+
 
     return (
 
@@ -117,8 +163,10 @@ const MainPanel = function(props) {
             {(renderPanel === "SimulatePanel") &&
              <SimulatePanel areaSize={{x: areaSizeX, y: areaSizeY}}
                             robotPath={robotPath}
+                            robotRotationDegree={robotRotationDegree}
                             itemsToRender={itemsToRender}/> }
 
+            <button onClick={MoveRobot} style={{position:"absolute"}}/>
 
             <div className="Bottom">
 
