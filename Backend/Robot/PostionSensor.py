9b9e80c1a914977799bae8a97c92d5c985039fa9
tlsdla1235 <tlsdla1235@uos.ac.kr>
2023-11-20 19:11:49 +0900

    commit : positions sensor

--- Backend/Robot/PostionSensor.py
+++ Backend/Robot/PostionSensor.py
@@ -2,11 +2,15 @@ import sys
 sys.path.append('.')
 
 class PositionSensor:
-    def __init__(self, _RobotPosition = [0,0]):
+    def __init__(self, _RobotPosition = [0,0],_direction = 0):
         self._RobotPosition = _RobotPosition
-
+        self._direction = _direction # 0, 1, 2, 3  : up, right, down, left
+        
     def get_position(self):
         return self._RobotPosition
+    
+    def get_direction(self):
+        return self._direction
     
     def DetectPosition(self):
         print("DetectPostion Test")
