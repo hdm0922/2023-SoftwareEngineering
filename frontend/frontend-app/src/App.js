f14724e1b973f2d0505456731cb625179d890f68
hdm0922 <dmh0922@naver.com>
2023-11-30 12:41:55 +0900

    feat: Show objects created by user STT

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -16,19 +16,7 @@ const App = function() {
     const [stateMessage, setStateMessage] = useState("음성 입력이 비활성화 되었습니다.");
     const [enableSTTButton, setEnableSTTButton] = useState(false);
 
-    const [areaSize,            setAreaSize]                    = useState({});
-    const [robotPath,           setRobotPath]                   = useState([]);
-    const [robotPosition,       setRobotPosition]               = useState({});
-    const [robotRotationDegree, setRobotRotationDegree]         = useState(0);
-    const [robotGoingCorrect,   setRobotGoingCorrect]           = useState(true);
     const [itemsToRender, setItemsToRender] = useState(Helper.Array2D(0, 0, {item:null, visible: false}));
-
-    const [hazardSensorNorth,       setHazardSensorNorth]       = useState(false);
-    const [colorBlobSensorNorth,    setColorBlobSensorNorth]    = useState(false);
-    const [colorBlobSensorEast,     setColorBlobSensorEast]     = useState(false);
-    const [colorBlobSensorWest,     setColorBlobSensorWest]     = useState(false);
-
-    const robotRotationDegreeReference = useRef(robotRotationDegree);
 
 
     const setSTTButtonState = function(newState) {
@@ -69,29 +57,7 @@ const App = function() {
             <MainPanel setSTTButtonState = {setSTTButtonState}
                        itemsToRender = {itemsToRender}
                        setItemsToRender = {setItemsToRender}
-                       updateItemsToRender = {updateItemsToRender}
-
-                       areaSize={areaSize}
-                       setAreaSize={setAreaSize}
-                       robotPath={robotPath}
-                       setRobotPath={setRobotPath}
-                       robotPosition={robotPosition}
-                       setRobotPosition={setRobotPosition}
-                       robotRotationDegree={robotRotationDegree}
-                       setRobotRotationDegree={setRobotRotationDegree}
-                       robotGoingCorrect={robotGoingCorrect}
-                       setRobotGoingCorrect={setRobotGoingCorrect}
-
-                       hazardSensorNorth={hazardSensorNorth}
-                       setHazardSensorNorth={setHazardSensorNorth}
-                       colorBlobSensorNorth={colorBlobSensorNorth}
-                       setColorBlobSensorNorth={setColorBlobSensorNorth}
-                       colorBlobSensorEast={colorBlobSensorEast}
-                       setColorBlobSensorEast={setColorBlobSensorEast}
-                       colorBlobSensorWest={colorBlobSensorWest}
-                       setColorBlobSensorWest={setColorBlobSensorWest}
-                       robotRotationDegreeReference={robotRotationDegreeReference}
-                       />
+                       updateItemsToRender = {updateItemsToRender}/>
 
 
 
