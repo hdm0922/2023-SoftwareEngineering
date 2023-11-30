13591424e5f9e220085abc11b3f3824cf38c233f
hdm0922 <dmh0922@naver.com>
2023-11-30 12:27:45 +0900

    test: Test rendering invisible objects

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 import MainPanel from './mainpanel/MainPanel';
 import BottomPanel from './bottompanel/BottomPanel';
+import Helper from './Helper';
 
 import { BiSolidJoystickButton } from "react-icons/bi";
 import { TbCircleLetterA } from "react-icons/tb";
@@ -10,23 +11,24 @@ import { TbCircleLetterB } from "react-icons/tb";
 import { FaCircle } from "react-icons/fa";
 
 
-
-// import Parser from './Parser';
-// const App = function() {
-
-//     const click = function() {
-//         const Message = "위험지역 둘 하나"
-//         console.log( Parser.parseUserSTT(Message) );
-//     };
-
-//     return <button onClick={click}>CLICK</button>
-
-// };
-
-const App = () => {
+const App = function() {
 
     const [stateMessage, setStateMessage] = useState("음성 입력이 비활성화 되었습니다.");
     const [enableSTTButton, setEnableSTTButton] = useState(false);
+
+    const [areaSize,            setAreaSize]                    = useState({});
+    const [robotPath,           setRobotPath]                   = useState([]);
+    const [robotPosition,       setRobotPosition]               = useState({});
+    const [robotRotationDegree, setRobotRotationDegree]         = useState(0);
+    const [robotGoingCorrect,   setRobotGoingCorrect]           = useState(true);
+    const [itemsToRender, setItemsToRender] = useState(Helper.Array2D(0, 0, {item:null, visible: false}));
+
+    const [hazardSensorNorth,       setHazardSensorNorth]       = useState(false);
+    const [colorBlobSensorNorth,    setColorBlobSensorNorth]    = useState(false);
+    const [colorBlobSensorEast,     setColorBlobSensorEast]     = useState(false);
+    const [colorBlobSensorWest,     setColorBlobSensorWest]     = useState(false);
+
+    const robotRotationDegreeReference = useRef(robotRotationDegree);
 
 
     const setSTTButtonState = function(newState) {
@@ -52,6 +54,11 @@ const App = () => {
         }
     };
 
+    const updateItemsToRender = function(itemType, x, y, visibleToRobot) {
+        const newItemsToRender = [...itemsToRender];
+        newItemsToRender[y][x] = {item: itemType, visible: visibleToRobot};
+        setItemsToRender(newItemsToRender);
+    }
 
 
 
@@ -59,13 +66,39 @@ const App = () => {
 
         <div className='gameBoyPanel'>
 
-            <MainPanel setSTTButtonState =   {setSTTButtonState}/>
+            <MainPanel setSTTButtonState = {setSTTButtonState}
+                       itemsToRender = {itemsToRender}
+                       setItemsToRender = {setItemsToRender}
+                       updateItemsToRender = {updateItemsToRender}
+
+                       areaSize={areaSize}
+                       setAreaSize={setAreaSize}
+                       robotPath={robotPath}
+                       setRobotPath={setRobotPath}
+                       robotPosition={robotPosition}
+                       setRobotPosition={setRobotPosition}
+                       robotRotationDegree={robotRotationDegree}
+                       setRobotRotationDegree={setRobotRotationDegree}
+                       robotGoingCorrect={robotGoingCorrect}
+                       setRobotGoingCorrect={setRobotGoingCorrect}
+
+                       hazardSensorNorth={hazardSensorNorth}
+                       setHazardSensorNorth={setHazardSensorNorth}
+                       colorBlobSensorNorth={colorBlobSensorNorth}
+                       setColorBlobSensorNorth={setColorBlobSensorNorth}
+                       colorBlobSensorEast={colorBlobSensorEast}
+                       setColorBlobSensorEast={setColorBlobSensorEast}
+                       colorBlobSensorWest={colorBlobSensorWest}
+                       setColorBlobSensorWest={setColorBlobSensorWest}
+                       robotRotationDegreeReference={robotRotationDegreeReference}
+                       />
 
 
 
             <BottomPanel setSTTButtonState              =   {setSTTButtonState}
                          stateMessage                   =   {stateMessage}
                          buttonDisabled                 =   {!enableSTTButton}
+                         updateItemsToRender            =   {updateItemsToRender}
             />
 
             <div className='logo'>Team OHDONGDONG</div>
